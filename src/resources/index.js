e2b451d4ceb1f322bf51b41d7cf3c447fd9a0858
John Tomaselli <jrt@gtz.com>
2018-08-24 15:59:25 -0400

    Removed src/resources/index.js

--- src/resources/index.js
+++
@@ -1,7 +0,0 @@
-export function configure(config) {
-  config.globalResources([
-        './elements/nav-bar/nav-bar.html',
-    './value-converters/number-value-converter',
-    './value-converters/stringify'
-  ]);
-}
