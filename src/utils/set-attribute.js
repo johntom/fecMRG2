893e1be3eb97a8f0384d0ef14ed2de5623457680
John Tomaselli <jrt@gtz.com>
2018-08-24 15:59:36 -0400

    Removed src/utils/set-attribute.js

--- src/utils/set-attribute.js
+++
@@ -1,9 +0,0 @@
-import {DataAttributeObserver} from 'aurelia-binding';
-
-export class SetAttributeBindingBehavior {
-  bind(binding, source) {
-    binding.targetObserver = new DataAttributeObserver(binding.target, binding.targetProperty);
-  }
-
-  unbind(binding, source) {}
-}
