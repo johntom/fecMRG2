126a31007d7c326f856f3f1f6a3811b9ea225b86
John Tomaselli <jrt@gtz.com>
2018-08-24 22:49:19 -0400

    betab5

--- src/main.js
+++ src/main.js
@@ -28,11 +28,14 @@ export async function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-     .feature('src/resources')
+    // .feature('src/resources')
     .plugin('aurelia-dialog')
     .plugin('aurelia-bootstrap-select');
   await aurelia.start();
-  await aurelia.setRoot('app');
+  // await aurelia.setRoot('app');
+  // aurelia.start().then(a => a.setRoot('src/views/shell/shell'));
+  await aurelia.setRoot('views/shell/shell');
+  
 }
 
 
