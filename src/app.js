a3be229371eb051002de469b65148d439ba4e0ac
John Tomaselli <jrt@gtz.com>
2018-08-25 12:00:02 -0400

    201

--- src/app.js
+++ src/app.js
@@ -1,64 +1,55 @@
-
-// export class App {
-//   configureRouter(config, router) {
-//     config.title = 'Aurelia Babel SystemJS';
-//     config.map([
-//       {"route": ["", "welcome"], "name": "welcome", "moduleId": "./views/welcome/welcome", "nav": true, "title": "Welcome"},
-//       {"route": "bootstrap-select", "name": "bootstrap-select", "moduleId": "./views/bootstrap-select/bootstrap-select", "nav": true, "title": "Bootstrap Select"}
-//       // {"route": "bootstrap-select-demo", "name": "bootstrap-select-demo", "moduleId": "./views/bootstrap-select-demo/bootstrap-select-demo", "nav": true, "title": "Bootstrap Select Demo"}
-//     ]);
-
-//     this.router = router;
-//   }
-// }
+// see Home NOT IN USE
 import { PLATFORM } from 'aurelia-pal';
 import { ApplicationService } from './services/application-service';
 import { AuthorizeStep } from './services/authorize-step';
 
 export class App {
-   static inject = [ApplicationService];
+  static inject = [ApplicationService];
 
   constructor(appService) {
     this.appService = appService;
   }
   configureRouter(config, router) {
-   config.title = 'MRG Layout';
+    config.title = 'MRG Layout';
     config.addPipelineStep('authorize', AuthorizeStep);
     config.map([
       // { "route": ["", "welcome"], "name": "welcome", "moduleId": PLATFORM.moduleName("./welcome"), "nav": true, "title": "Welcome" },
       { "route": '', redirect: 'home' },
       { "route": 'home', name: 'home', moduleId: PLATFORM.moduleName('./views/home/home'), nav: true, title: 'Home' },
-    
-      { "route": 'inventory/:id', name: 'inventory-search-results', "moduleId":PLATFORM.moduleName('./views/inventory/search-results'), title: 'Search Results' },
-      { "route": 'inventory/data/:id', name: 'inventory-data-form', "moduleId":PLATFORM.moduleName('./views/inventory/data-form'), title: 'Data Form' },
+      { "route": 'inventory/:id', name: 'inventory-search-results', "moduleId": PLATFORM.moduleName('./views/inventory/search-results'), title: 'Search Results' },
+      { "route": 'inventory/data/:id', name: 'inventory-data-form', "moduleId": PLATFORM.moduleName('./views/inventory/data-form'), title: 'Data Form' },
       { "route": 'inventory', name: 'inventory', "moduleId": PLATFORM.moduleName('./views/inventory/inventory'), nav: true, title: 'Inventory' },
+      { "route": 'contact', name: 'contact', moduleId: PLATFORM.moduleName('./views/contact/contact'), nav: true, title: 'Contact' },
+      { "route": 'contact/:id', name: 'contact-search-results', moduleId: PLATFORM.moduleName('./views/contact/search-results'), title: 'Search Results' },
+      { "route": 'contact/data/:id', name: 'contact-data-form', moduleId: PLATFORM.moduleName('./views/contact/data-form'), title: 'Data Form' },
+      { "route": 'catalog', name: 'catalog', moduleId: PLATFORM.moduleName('./views/catalog/catalog'), nav: true, title: 'Catalog' },
+      { "route": 'catalog/:id', name: 'catalog-search-results', moduleId: PLATFORM.moduleName('./views/catalog/search-results'), title: 'Search Results' },
+      { "route": 'catalog/data/:id', name: 'catalog-data-form', moduleId: PLATFORM.moduleName('./views/catalog/data-form'), title: 'Data Form' },
+      { "route": 'savedlists', name: 'savedlists', moduleId: PLATFORM.moduleName('./views/savedlists/savedlists'), nav: true, title: 'Saved Lists' },
+      { "route": 'savedlists/:id', name: 'contact-detail', moduleId: PLATFORM.moduleName('./views/savedlists/contact-detail'), href: 'contact/123', nav: true, title: 'Contact Detail' },
+      { "route": 'action', name: 'action', moduleId: PLATFORM.moduleName('./views/action/action'), nav: true, title: 'Actions' },
+      { "route": 'action/:id', name: 'action-search-results', moduleId: PLATFORM.moduleName('./views/action/search-results'), title: 'Search Results' },
+      { "route": 'action/data/:id', name: 'action-data-form', moduleId: PLATFORM.moduleName('./views/action/data-form'), title: 'Data Form' },
 
-       { route: 'contact', name: 'contact', moduleId: './views/contact/contact', nav: true, title: 'Contact' },
-      { route: 'contact/:id', name: 'contact-search-results', moduleId: './views/contact/search-results', title: 'Search Results' },
-      { route: 'contact/data/:id', name: 'contact-data-form', moduleId: './views/contact/data-form', title: 'Data Form' },
-
-    //  { "route": "grid", "name": "grid", "moduleId": PLATFORM.moduleName("./views/grid/grid"), "nav": true, "title": "Imates-Srv-Inv Grid" },
-     
+      // { route: '', name: 'no-selection',      moduleId: './no-selection',      nav: true, title: 'Select' },
+      //  { "route": "grid", "name": "grid", "moduleId": PLATFORM.moduleName("./views/grid/grid"), "nav": true, "title": "Imates-Srv-Inv Grid" },
       // { "route": "gridservice", "name": "gridservice", "moduleId": PLATFORM.moduleName("./views/gridservice/grid"), "nav": true, "title": "Service Grid" },
       // { "route": "inmates", "name": "inmates", "moduleId": PLATFORM.moduleName("./views/inmates/inmates"), "nav": true, "title": "Inmates" },
       // { "route": "inmatesdata", "name": "inmatesdata", "moduleId": PLATFORM.moduleName("./views/inmates/data-form"), "nav": false, "title": "InmatesData" },
       // { "route": "inmates/:id", "name": "inmates-data-form", "moduleId": PLATFORM.moduleName("./views/inmates/data-form"), "nav": false, "title": "InmatesData" },
       // { "route": "payee", "name": "payee", "moduleId": PLATFORM.moduleName("./views/payee/payee"), "nav": true, "title": "Payee" },
-
-
       { "route": "ab-select", "name": "ab-select", "moduleId": PLATFORM.moduleName("./views/ab-select/ab-select"), "nav": true, "title": "AB Select" }
-
 
     ]);
 
     this.router = router;
   }
-  
+
   selectTab(e, tab) {
     e.preventDefault();
     e.stopPropagation();
- // by calling router if canDeactivate is false it wont allow route
- // the canDeactivate raises a dialog
+    // by calling router if canDeactivate is false it wont allow route
+    // the canDeactivate raises a dialog
     this.appService.navigate(tab.href);
     return true;
   }
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -7,7 +7,7 @@ import { MyDataService } from "../../services/my-data-service";
 
 export class Home {
   heading = "Welcome to the MRG Home page!";
-  version = 200
+  version = 201
   constructor(appService, dataService) {
     this.appService = appService;
     this.dataService = dataService;
