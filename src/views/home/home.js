cf9fab7af13555d694b5da2145aa41393ffe2dc0
John Tomaselli <jrt@gtz.com>
2018-08-29 16:44:33 -0400

    207

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -7,7 +7,7 @@ import { MyDataService } from "../../services/my-data-service";
 
 export class Home {
   heading = "Welcome to the MRG Home page!";
-  version = 206
+  version = 207
   constructor(appService, dataService) {
     this.appService = appService;
     this.dataService = dataService;
