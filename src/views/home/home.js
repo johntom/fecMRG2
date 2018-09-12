667ac3e364cc6f1cb8deab39b52709f47bd24927
John Tomaselli <jrt@gtz.com>
2018-09-12 16:56:23 -0400

    240.7

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -6,7 +6,7 @@ import { MyDataService } from "../../services/my-data-service";
 
 export class Home {
   heading = "Welcome to the MRG Home page!";
-  version = 240.6
+  version = 240.7
   versiondate=Date();
   issues=[
   'Make tabs like mas w/scrollbar with sticky header',
