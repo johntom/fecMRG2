5826e53e9cd43844ba25e96ce7fd43e740a052f4
John Tomaselli <jrt@gtz.com>
2018-10-27 20:39:18 -0400

    241.29

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -6,7 +6,7 @@ import { MyDataService } from "../../services/my-data-service";
 
 export class Home {
   heading = "MRG Home Page";
-  version = 241.28
+  version = 241.29
   versionDate = 'Sat 10/27/2018'
   // versiondate=Date();
   //  "select2": {
