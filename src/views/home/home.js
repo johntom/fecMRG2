fed62315fa0622f68e51e66914fe2a6555ec4319
John Tomaselli <jrt@gtz.com>
2019-02-15 14:46:10 -0500

    242.73

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -20,7 +20,7 @@ import { ApiService } from '../../utils/servicesApi';
 
 export class Home {
   heading = "MRG Home Page";
-  version = 242.71
+  version = 242.73
   versionDate = 'Fri 2/15/2019'
 
 
