946a7d7036adfd3b9cb3f5da1c1b3c3c96c2a69e
John Tomaselli <jrt@gtz.com>
2019-02-15 20:59:36 -0500

    ci2

--- src/resources/value-converters/conv-inch2cm.js
+++ src/resources/value-converters/conv-inch2cm.js
@@ -2,18 +2,13 @@ import { valueConverter } from 'aurelia-framework';
 
 @valueConverter("convInch2cm")
 export class convInch2cmValueConverter {
-
   toView(value) {
     console.log('value', value)
-
     if (value === undefined || value === '') {
       value = 'none';
     }
     (value !== 'none') ? value = value+' in '+ value * 2.54 + ' cm ' : value
-
-
     return value
-
   }
 }
 
