2f11540f1452bbb7dc0b678cafb7ebc5e922f258
John Tomaselli <jrt@gtz.com>
2019-02-18 14:09:42 -0500

    try12

--- src/resources/attributes/select2/select2-attribute.js
+++ src/resources/attributes/select2/select2-attribute.js
@@ -3,45 +3,33 @@ import {autoinject, bindable, bindingMode, customAttribute} from "aurelia-framew
 @customAttribute("select2")
 @autoinject()
 export class Select2Attribute {
-    @bindable({ defaultBindingMode: bindingMode.twoWay }) value: any;
-    @bindable({ defaultBindingMode: bindingMode.oneTime }) allowClear: boolean = false;
+    @bindable({ defaultBindingMode: bindingMode.twoWay }) value;
+    @bindable({ defaultBindingMode: bindingMode.oneTime }) allowClear = false;
+    select;
+    @bindable( {  defaultBindingMode: bindingMode.twoWay}) value;
+    @bindable({ defaultBindingMode: bindingMode.oneTime }) allowClear = false;
 
-    // private select: any;
-
-select: any;
-	//{
-  @bindable( {  defaultBindingMode: bindingMode.twoWay})
-value: any;
-	
-      @bindable({ defaultBindingMode: bindingMode.oneTime })
- allowClear: boolean = false;
-
-
-
-    constructor(private element: Element) { }
-
+    constructor(element) { }
     attached() {
-        let clear = ((this.allowClear as any) === "true");
-        if (this.select) {
-            return;
-        }
-        this.select = $(this.element)
-            .select2({ allowClear: clear, placeholder: "Select an option..." });
-        this.select.on("change", (event: JQueryEventObject, options: any) => {
-            if (event.originalEvent) {
-                return;
-            }
-            if (this.value != (event.target).value) {
-                this.value = (event.target).value;
-            }
-        });
+      let clear = ((this.allowClear) === "true");
+      if (this.select) {
+          return;
+      }
+      this.select = $(this.element)
+          .select2({ allowClear: clear, placeholder: "Select an option..." });
+      this.select.on("change", (event, options) => {
+          if (event.originalEvent) {
+              return;
+          }
+          if (this.value != (event.target).value) {
+              this.value = (event.target).value;
+          }
+      });
     }
-
     valueChanged(newVal, oldVal) {
         if (this.select)
             this.select.trigger("change");
-    }
-    
+    }    
     detached() {
         if (this.select) {
             this.select.off("change");
