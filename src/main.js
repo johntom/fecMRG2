26c0edc0685122754ec3e0da2a0103d8d5cb641a
John Tomaselli <jrt@gtz.com>
2019-02-18 13:34:02 -0500

    try6

--- src/main.js
+++ src/main.js
@@ -18,28 +18,27 @@ export async function configure(aurelia) {
       config.settings.startingZIndex = 5;
       config.settings.keyboard = true;
     })
-    //.plugin('aurelia-bootstrap')
-      .plugin('aurelia-bootstrap', config => {
-      config.options.accordionCloseOthers = true;
-      config.options.accordionGroupPanelClass = 'panel-default';
-      config.options.btnLoadingText = 'Loading...';
-      config.options.dropdownAutoClose = 'always';
-      config.options.paginationBoundaryLinks = false;
-      config.options.paginationDirectionLinks = true;
-      config.options.paginationFirstText = 'First';
-      config.options.paginationHideSinglePage = true;
-      config.options.paginationLastText = 'Last';
-      config.options.paginationNextText = '>';
-      config.options.paginationPreviousText = '<';
-      config.options.popoverPosition = 'top';
-      config.options.popoverTrigger = 'mouseover';
-      config.options.tabsetType = 'tabs';
-      config.options.tabsetVertical = false;
-      config.options.tooltipClass = 'tooltip';
-      config.options.tooltipPosition = 'top';
-      config.options.tooltipTrigger = 'mouseover';
-      })
-    .plugin('aurelia-validation');
+    //   .plugin('aurelia-bootstrap', config => {
+    //   config.options.accordionCloseOthers = true;
+    //   config.options.accordionGroupPanelClass = 'panel-default';
+    //   config.options.btnLoadingText = 'Loading...';
+    //   config.options.dropdownAutoClose = 'always';
+    //   config.options.paginationBoundaryLinks = false;
+    //   config.options.paginationDirectionLinks = true;
+    //   config.options.paginationFirstText = 'First';
+    //   config.options.paginationHideSinglePage = true;
+    //   config.options.paginationLastText = 'Last';
+    //   config.options.paginationNextText = '>';
+    //   config.options.paginationPreviousText = '<';
+    //   config.options.popoverPosition = 'top';
+    //   config.options.popoverTrigger = 'mouseover';
+    //   config.options.tabsetType = 'tabs';
+    //   config.options.tabsetVertical = false;
+    //   config.options.tooltipClass = 'tooltip';
+    //   config.options.tooltipPosition = 'top';
+    //   config.options.tooltipTrigger = 'mouseover';
+    //   })
+    // .plugin('aurelia-validation');
 
 
   await aurelia.start();
