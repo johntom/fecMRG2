af4731ff9ec236790fc5ebb1f36e7a79b0dbd60d
John Tomaselli <jrt@gtz.com>
2019-02-27 10:38:06 -0500

    300.35

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -7,7 +7,7 @@ import { ApiService } from '../../utils/servicesApi';
 
 export class Home {
   heading = "MRG Home Page";
-  version = 300.34 
+  version = 300.35  
   versionDate = 'Wed 2/27/2019'
  
   issues = [
