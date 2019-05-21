3fb3ed0a42d2e42d3f17a01709d983c866501df5
John Tomaselli <jrt@gtz.com>
2019-05-21 16:08:49 -0400

    301.53

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -7,10 +7,9 @@ import { ApiService } from '../../utils/servicesApi';
 
 export class Home {
   heading = "MRG Home Page";
-  version = 301.51
-  // versionDate = 'Mon 5/6/2019 11pm'
-
-  versionDate = 'Tue 5/21/2019 2pm'
+  version = 301.53
+ 
+  versionDate = 'Tue 5/21/2019 4pm'
 
   issues = [
     `dirty: "phones": [       "type": null" not type:0 {"phones.type":  0} fixContactType`  ,
