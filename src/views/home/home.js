034c865c5aa47e8c0eadd9aae763a5801a489eed
John Tomaselli <jrt@gtz.com>
2019-06-10 16:01:05 -0400

    301.74a

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -7,7 +7,7 @@ import { ApiService } from '../../utils/servicesApi';
 
 export class Home {
   heading = "MRG Home Page";
-  version = 301.74
+  version = 301.74a
  
   versionDate = 'Mon 6/10/2019 1pm'
 
