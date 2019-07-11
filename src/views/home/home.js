860f96b246665f0c43c4c1b1575c5686539c392f
John Tomaselli <jrt@gtz.com>
2019-07-11 13:03:10 -0400

    301.93

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -7,9 +7,9 @@ import { ApiService } from '../../utils/servicesApi';
 
 export class Home {
   heading = "MRG Home Page";
-  version = 301.92
+  version = 301.93
 
-  versionDate = 'Wed 7/10/2019 2pm'
+  versionDate = 'Thur 7/10/2019 2pm'
 
   issues = [
     'JUNE 15 CONV fix org/contacts save both cols before redo',
