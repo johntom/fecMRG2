70cfad1a65be7eaff68b8db548099a6492ef2490
John Tomaselli <jrt@gtz.com>
2019-08-23 20:06:02 -0400

    303.65.39

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
-// import 'babel-polyfill';
-// need next 2 lines for aurelia-bootstrap-select
-// import 'jquery';
-// import 'bootstrap';
-// import 'kendo/css/web/kendo.common.min.css';
-// import 'kendo/css/web/kendo.default.min.css';
-// import "kendo/js/kendo.datepicker.js";
-// import "kendo/js/kendo.grid.js";
-// import "kendo/js/kendo.editor.js";
-// import "kendo/js/kendo.multiselect.js";
+
 
 export async function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-    // .plugin('aurelia-kendoui-bridge')
-      .plugin('aurelia-kendoui-bridge', (kendo) => kendo.detect().notifyBindingBehavior())
-     // .plugin('aurelia-kendoui-bridge', (kendo) => kendo.pro())
+       .plugin('aurelia-kendoui-bridge', (kendo) => kendo.detect().notifyBindingBehavior())
+       .plugin('aurelia-dialog', config => {
+      config.useDefaults();
+      config.settings.lock = true;
+      config.settings.centerHorizontalOnly = false;
+      config.settings.startingZIndex = 5;
+      config.settings.keyboard = true;
+    })
+  await aurelia.start();
+  await aurelia.setRoot('app');
+ }
+
+
+
+   // .plugin('aurelia-kendoui-bridge', (kendo) => kendo.pro())
    
     // .globalResources( "aurelia-kendoui-bridge/datepicker/datepicker" )
     // .globalResources( "aurelia-kendoui-bridge/grid/grid" )
@@ -38,88 +41,13 @@ export async function configure(aurelia) {
 //     config.options.gridMenu.iconCssClass = 'fa fa-bars';
 //   })
 
-
-    .plugin('aurelia-dialog', config => {
-      config.useDefaults();
-      config.settings.lock = true;
-      config.settings.centerHorizontalOnly = false;
-      config.settings.startingZIndex = 5;
-      config.settings.keyboard = true;
-    })
-  await aurelia.start();
-  await aurelia.setRoot('app');
- }
-
 // import 'babel-polyfill';
-// import * as Bluebird from 'bluebird';
-
+// need next 2 lines for aurelia-bootstrap-select
 // import 'jquery';
 // import 'bootstrap';
-
-// export async function configure(aurelia) {
-//   aurelia.use
-//     .standardConfiguration()
-//     .developmentLogging()
-//     // .feature('src/resources')
-//     // .plugin('aurelia-dialog')
-//     .plugin('aurelia-bootstrap-select')
-//     .plugin('aurelia-dialog', config => {
-//       config.useDefaults();
-//       config.settings.lock = true;
-//       config.settings.centerHorizontalOnly = false;
-//       config.settings.startingZIndex = 5;
-//       config.settings.keyboard = true;
-//     })
-//     //   .plugin('aurelia-bootstrap', config => {
-//     //   config.options.accordionCloseOthers = true;
-//     //   config.options.accordionGroupPanelClass = 'panel-default';
-//     //   config.options.btnLoadingText = 'Loading...';
-//     //   config.options.dropdownAutoClose = 'always';
-//     //   config.options.paginationBoundaryLinks = false;
-//     //   config.options.paginationDirectionLinks = true;
-//     //   config.options.paginationFirstText = 'First';
-//     //   config.options.paginationHideSinglePage = true;
-//     //   config.options.paginationLastText = 'Last';
-//     //   config.options.paginationNextText = '>';
-//     //   config.options.paginationPreviousText = '<';
-//     //   config.options.popoverPosition = 'top';
-//     //   config.options.popoverTrigger = 'mouseover';
-//     //   config.options.tabsetType = 'tabs';
-//     //   config.options.tabsetVertical = false;
-//     //   config.options.tooltipClass = 'tooltip';
-//     //   config.options.tooltipPosition = 'top';
-//     //   config.options.tooltipTrigger = 'mouseover';
-//     //   })
-//     // .plugin('aurelia-validation');
-
-
-//   await aurelia.start();
-//   await aurelia.setRoot('app');
-//   //// aurelia.start().then(a => a.setRoot('src/views/shell/shell'));
-//   //or
-
-//   // await aurelia.setRoot('views/shell/shell');
-
-// }
-/** .plugin('aurelia-bootstrap', config => {
-      config.options.accordionCloseOthers = true;
-      config.options.accordionGroupPanelClass = 'panel-default';
-      config.options.btnLoadingText = 'Loading...';
-      config.options.dropdownAutoClose = 'always';
-      config.options.paginationBoundaryLinks = false;
-      config.options.paginationDirectionLinks = true;
-      config.options.paginationFirstText = 'First';
-      config.options.paginationHideSinglePage = true;
-      config.options.paginationLastText = 'Last';
-      config.options.paginationNextText = '>';
-      config.options.paginationPreviousText = '<';
-      config.options.popoverPosition = 'top';
-      config.options.popoverTrigger = 'mouseover';
-      config.options.tabsetType = 'tabs';
-      config.options.tabsetVertical = false;
-      config.options.tooltipClass = 'tooltip';
-      config.options.tooltipPosition = 'top';
-      config.options.tooltipTrigger = 'mouseover'; 
-     
-      */
-
+// import 'kendo/css/web/kendo.common.min.css';
+// import 'kendo/css/web/kendo.default.min.css';
+// import "kendo/js/kendo.datepicker.js";
+// import "kendo/js/kendo.grid.js";
+// import "kendo/js/kendo.editor.js";
+// import "kendo/js/kendo.multiselect.js";
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -7,7 +7,7 @@ import { ApiService } from '../../utils/servicesApi';
  
 export class Home { 
   heading = "MRG Home Page";
-  version = ' 303.65.38 ' 
+  version = ' 303.65.39 ' 
   versionDate = 'Fri 8/21/2019 2pm'
   prevversion = ' 303.52 ' 
   prevversionDate = 'Mon 8/5/2019 5pm'
