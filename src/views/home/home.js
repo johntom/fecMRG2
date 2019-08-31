368193670624375f342b7ce0920092d964eb9d14
John Tomaselli <jrt@gtz.com>
2019-08-30 22:02:59 -0400

    304.09

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -7,7 +7,7 @@ import { ApiService } from '../../utils/servicesApi';
 
 export class Home {
   heading = "MRG Home Page";
-  version = ' 304.09 '
+  version = ' 304.10 ' 
   versionDate = 'Fri 8/30/2019 10pm'
   prevversion = ' 303.52 '
   prevversionDate = 'Mon 8/5/2019 5pm'
