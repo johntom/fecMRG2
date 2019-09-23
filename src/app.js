e26a953e13817624bdfe035c3e29db4f604ff1ef
John Tomaselli <jrt@gtz.com>
2019-09-23 12:38:04 -0400

    405.55

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ export class App {
     this.eventAggregator = eventAggregator;
     this.secured = false
     this.user='jrt'
-    this.appService.version='405.54b'   
+    this.appService.version='405.55'   
   }
  
   // use activationStrategy for all wildcards 
