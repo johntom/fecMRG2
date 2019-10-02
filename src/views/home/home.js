004bcdb9b622afb11d794959cd02db950fcea5f8
John Tomaselli <jrt@gtz.com>
2019-10-02 14:53:47 -0400

    303.55

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -12,7 +12,7 @@ export class Home {
   heading = "MRG HOME PAGE";
   footer = 'DataForm FOOTER...'
   // version = ' 305.53g '   
-  versionDate = 'Sat 9/28/2019 9am'
+  versionDate = 'Tue 10/2/2019 3pm'
   prevversion = ' 303.55 '
   prevversionDate = 'Mon 8/5/2019 5pm'
   newversion = 'none'
