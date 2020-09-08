7544924c04edf7a229c9ef55a97c661eb2688462
John Tomaselli <jrt@gtz.com>
2020-09-08 16:06:36 -0400

    406.32

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ export class App {
     this.secured = false
     // this.user='jrt0'
     // this.password='111'
-    this.appService.version = 's406.32' 
+    this.appService.version = '406.32' 
   }
 
   // use activationStrategy for all wildcards 
